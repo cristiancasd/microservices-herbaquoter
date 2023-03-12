59b2dafc496da44043a5dc76487554db4cb65b50
Cristian Salazar <cristiancasd@gmail.com>
2023-03-12 13:11:30 -0500

    gitactions, split k8s manifest

--- herba-prod-auth/src/auth/dto/update-auth.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { IsOptional, IsString, Matches, MaxLength, MinLength } from 'class-validator';
-import { CreateUserDto } from './create-user.dto';
-export class UpdateAuthDto extends PartialType(CreateUserDto) {
-}
